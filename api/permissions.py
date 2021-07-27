45d838dccf74ce4b4d90a755e941b77eb14a9566
AllStars123 <purnov.nikita@yandex.ru>
2021-07-27 19:32:10 +0300

    fix bug

--- api/permissions.py
+++ api/permissions.py
@@ -3,12 +3,9 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAdminOrUser(BasePermission):
     def has_permission(self, request, view):
-        try:
-            user = view.get_object()
-            if user == request.user and request.method in ['PATCH', 'PUT']:
-                return True
-        except Exception:
-            pass
+        user = view.get_object()
+        if user == request.user and request.method in ['PATCH', 'PUT']:
+            return True
         if request.method in SAFE_METHODS:
             return True
         return request.user.is_staff
--- api/serializers.py
+++ api/serializers.py
@@ -16,3 +16,10 @@ class UserSerializer(serializers.ModelSerializer):
         user.set_password(password)
         user.save()
         return user
+
+    def update(self, instance, validated_data):
+        if validated_data.get('password'):
+            instance.set_password(validated_data.pop('password'))
+        instance.update(**validated_data)
+        instance.save()
+        return instance
